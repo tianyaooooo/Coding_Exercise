48599f32f59df4753f0af64bc332a4fedb1dbd8a
tianyaooooo <43740253+tianyaooooo@users.noreply.github.com>
2020-08-04 14:33:09 +0800

    Update 9-4设计MyTime类.cpp

--- 9th_week/9-4设计MyTime类.cpp
+++ 9th_week/9-4设计MyTime类.cpp
@@ -1,30 +1,30 @@
 /*
-��Ŀ���ݣ�
+题目内容：
 
-���һ��MyTime�࣬��Ա����SetTime()����ʱ�䣬print_12()��12��0-11��Сʱ����ʾʱ�䣨AM���磬PM���磩��print_24()��24��0-23��Сʱ����ʾʱ�䡣
-
-
-�����ʽ:
-
-��������ʱ���ʱ���֡���
+设计一个MyTime类，成员函数SetTime()设置时间，print_12()以12（0-11）小时制显示时间（AM上午，PM下午），print_24()以24（0-23）小时制显示时间。
 
 
-�����ʽ��
+输入格式:
 
-����12Сʱ�ƺ�24Сʱ��������ʾʱ��,ע��ʱ���ʽ�е�ð����Ӣ��ð��,ʱ���붼����λ��AM,PMǰ��һ���ո�����12:00��00:00:00 AM������ʮ������00:00:00 PM�� 
+所需设置时间的时、分、秒
 
 
-����������
+输出格式：
+
+按照12小时制和24小时制依次显示时间,注意时间格式中的冒号是英文冒号,时分秒都是两位，AM,PM前有一个空格，晚上12:00是00:00:00 AM，中午十二点是00:00:00 PM。 
+
+
+输入样例：
 
 13 23 34
 
 
-���������
+输出样例：
 
 01:23:34 PM
 13:23:34
 
-ʱ�����ƣ�500ms�ڴ����ƣ�32000kb
+时间限制：500ms内存限制：32000kb
 */
 
 #include<iostream>
@@ -44,7 +44,7 @@ class MyTime
 		void print_24();
 };
 
-// �ٶ������ʱ���Ǻ����ģ���Ŀû��˵��������ô�죩����û��������Ч�Լ��飬����û�� 77 88 99 
+// 假定输入的时间是合理的（时0-23， 分0-59，秒0-59，因为题目没有说不合理怎么办），故没有设置有效性检验，例如没有 77 88 99 
 void MyTime::SetTime(int myhour, int myminute, int mysecond)
 {
 	hour = myhour;
@@ -69,14 +69,10 @@ void MyTime::print_12()
 		cout<<0<<hour - 12;
 		flag = 1; 
 	}
-	else if (hour > 21 && hour < 24)
+	else
 	{
 		cout<<hour - 12;
 		flag = 1;
-	}
-	else
-	{
-		cout<<0<<0; 
 	}
 	cout<<':';
 	if (minute <= 9)
@@ -129,7 +125,7 @@ void MyTime::print_24()
 	cout<<':';
 	if (second <= 9)
 	{
-		cout<<0<<second;
+		cout<<0<<second<<endl;
 	}
 	else
 	{
